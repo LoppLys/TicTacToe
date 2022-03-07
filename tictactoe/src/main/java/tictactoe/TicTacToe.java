38f6c826104869d2a59fadcc0a75e92b7a35bd21
Simon Täcklind <simon.tacklind@gmail.com>
2022-03-07 14:15:04 +0100

    ändringar

--- tictactoe/src/main/java/tictactoe/TicTacToe.java
+++ tictactoe/src/main/java/tictactoe/TicTacToe.java
@@ -3,149 +3,116 @@ package tictactoe;
 import java.util.Scanner;
 
 public class TicTacToe {
-    private char[][] gameBoard = {
-    {'|' ,' ',' ',' ', '|', ' ',' ',' ', '|', ' ',' ',' ', '|', ' ',' ',' ', '|', ' ',' ',' ', '|'}, 
-    {'-','-','-','-','+','-','-','-','+','-','-','-','+','-','-','-','+','-','-','-','-'},
-    {'|' ,' ',' ',' ', '|', ' ',' ',' ', '|', ' ',' ',' ', '|', ' ',' ',' ', '|', ' ',' ',' ', '|'},
-    {'-','-','-','-','+','-','-','-','+','-','-','-','+','-','-','-','+','-','-','-','-'},
-    {'|' ,' ',' ',' ', '|', ' ',' ',' ', '|', ' ',' ',' ', '|', ' ',' ',' ', '|', ' ',' ',' ', '|'}, 
-    {'-','-','-','-','+','-','-','-','+','-','-','-','+','-','-','-','+','-','-','-','-'},
-    {'|' ,' ',' ',' ', '|', ' ',' ',' ', '|', ' ',' ',' ', '|', ' ',' ',' ', '|', ' ',' ',' ', '|'},
-    {'-','-','-','-','+','-','-','-','+','-','-','-','+','-','-','-','+','-','-','-','-'},
-    {'|' ,' ',' ',' ', '|', ' ',' ',' ', '|', ' ',' ',' ', '|', ' ',' ',' ', '|', ' ',' ',' ', '|'}};
-    
+    private char[][] gameBoard = {  {' ', ' ', ' ', ' ', ' '},
+                                    {' ', ' ', ' ', ' ', ' '},
+                                    {' ', ' ', ' ', ' ', ' '},
+                                    {' ', ' ', ' ', ' ', ' '},
+                                    {' ', ' ', ' ', ' ', ' '}};
+    private boolean gameOver = false;
 
     public TicTacToe(){
         run();
     }
 
     private void run(){
-        printGameBoard();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter first placement (1-25)");
-        int pos = scanner.nextInt();
-        printHumanChoice(pos);
+        while(!gameOver) {
+            printGameBoard();
+            System.out.println("Where do you want to put a marker? x y");
+            int x = scanner.nextInt() - 1;
+            int y = scanner.nextInt() - 1;
+            printHumanChoice(x, y);
+            printComputerChoice();
+        }
         scanner.close();
     }
 
     private void printGameBoard(){
-        for(char[] row : gameBoard){
-            for(char column : row){
-                System.out.print(column);
+        for(int i = 0; i < gameBoard.length; i++){
+            for(int j = 0; j < gameBoard[i].length; j++){
+                System.out.printf("| %c ", gameBoard[i][j]);
             }
-            System.out.println(); //new line
+            System.out.println("|");
+            if(i != gameBoard.length-1) {
+                System.out.println("+---+---+---+---+---+");
+            }
         }
     }
 
-    private void printHumanChoice(int pos){
-        switch(pos){
-            case 1:
-            gameBoard[0][2] = 'X';
-            run();
-                break;
-            case 2:
-            gameBoard[0][6] = 'X';
-            run();
-                break;
-            case 3:
-            gameBoard[0][10] = 'X';
-            run();
-                break;
-            case 4:
-            gameBoard[0][14] = 'X';
-            run();
-                break;
-            case 5:
-            gameBoard[0][18] = 'X';
-            run();
-                break;
-            case 6:
-            gameBoard[2][2] = 'X';
-            run();
-                break;
-            case 7:
-            gameBoard[2][6] = 'X';
-            run();
-                break;
-            case 8:
-            gameBoard[2][10] = 'X';
-            run();
-                break;
-            case 9:
-            gameBoard[2][14] = 'X';
-            run();
-                break;
-            case 10:
-            gameBoard[2][18] = 'X';
-            run();
-                break;
-            case 11:
-            gameBoard[4][2] = 'X';
-            run();
-                break;
-            case 12:
-            gameBoard[4][6] = 'X';
-            run();
-                break;
-            case 13:
-            gameBoard[4][10] = 'X';
-            run();
-                break;
-            case 14:
-            gameBoard[4][14] = 'X';
-            run();
-                break;
-            case 15:
-            gameBoard[4][18] = 'X';
-            run();
-                break;
-            case 16:
-            gameBoard[6][2] = 'X';
-            run();
-                break;
-            case 17:
-            gameBoard[6][6] = 'X';
-            run();
-                break;
-            case 18:
-            gameBoard[6][10] = 'X';
-            run();
-                break;
-            case 19:
-            gameBoard[6][14] = 'X';
-            run();
-                break;
-            case 20:
-            gameBoard[6][18] = 'X';
-            run();
-                break;
-            case 21:
-            gameBoard[8][2] = 'X';
-            run();
-                break;
-            case 22:
-            gameBoard[8][6] = 'X';
-            run();
-                break;
-            case 23:
-            gameBoard[8][10] = 'X';
-            run();
-                break;
-            case 24:
-            gameBoard[8][14] = 'X';
-            run();
-                break;
-            case 25:
-            gameBoard[8][18] = 'X';
-            run();
-                break;
-
+    private void printHumanChoice(int x, int y){
+        try {
+            if (gameBoard[y][x] == ' ') {
+                gameBoard[y][x] = 'X';
+                checkIfPlayerWon(x, y);
+            }
+            else{
+                System.out.println("That spot is already taken you crazy human!");
+            }
         }
+        catch(Exception e){
+            System.out.println("Please try to keep your markers INSIDE the board.");
+        }
+    }
+
+    private void printComputerChoice(){
+        // Do minimax stuff to place computer player marker.
+
+        // Do check to see if computer player has won.
+    }
+
+    private void checkIfPlayerWon(int x, int y){
+        if(checkRow(y, 'X') || checkColumn(x, 'X') || checkDiagonal(x, y, 'X')){
+            printGameBoard();
+            System.out.println("The human has won, i cant believe it.");
+            gameOver = true;
+        }
+    }
+
+    private boolean checkRow(int y, char marker){
+        for(char c : gameBoard[y]){
+            if(c != marker){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean checkColumn(int x, char marker){
+        for(char[] c : gameBoard){
+            if(c[x] != marker){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean checkDiagonal(int x, int y, char marker){
+        if(x == y){
+            for(int i = 0; i < gameBoard.length; i++){
+                if(gameBoard[i][i] != marker){
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        else if(x+y == 4){
+            for(int i = 0; i < gameBoard.length; i++){
+                if(gameBoard[4-i][i] != marker){
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
     }
 
     public static void main(String[] args){
         TicTacToe game = new TicTacToe();
-        
     }
 }
 
