862153a544bbedc52f3262a24fb34fecfb6a4835
Simon Täcklind <simon.tacklind@gmail.com>
2022-03-08 17:08:17 +0100

    fix

--- tictactoe/src/main/java/tictactoe/TicTacToe.java
+++ tictactoe/src/main/java/tictactoe/TicTacToe.java
@@ -1,9 +1,7 @@
 package tictactoe;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class TicTacToe {
     private static final char HUMAN = 'X';
